2fb7c7da6b4014e034f4f91283efb4a06f4ebd36
Peter Holmberg <peter.hlmbrg@gmail.com>
2016-07-01 11:33:36 +0200

    Added package and brunch-config

---
+++ brunch-config.js
@@ -0,0 +1,34 @@
+exports.config = {
+  npm: {
+    enabled: true
+  },
+
+  sourceMaps: true,
+  overrides: {
+    production: {
+      sourceMaps: true
+    }
+  },
+
+  plugins: {
+    babel: {
+      presets: ['es2015', 'react', 'stage-1'],
+      pattern: /\.(es6|jsx|js)$/
+    }
+  },
+
+  files: {
+    javascripts: {
+      joinTo: 'javascripts/my-pages.js'
+    },
+    stylesheets: {
+      joinTo: 'stylesheets/my-pages.css'
+    }
+  },
+
+  server: {
+    hostname: '0.0.0.0',
+    port: 4444,
+    run: true
+  }
+};
